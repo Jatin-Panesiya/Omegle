1426e7c2063c61859dedfcf0d6084fb605efad67
apple <apple@apples-MacBook-Pro.local>
2024-01-09 10:02:45 +0530

    adapter added

--- src/app/layout.js
+++ src/app/layout.js
@@ -14,6 +14,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.1.4/adapter.min.js"></script>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   )
