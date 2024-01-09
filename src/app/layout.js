cd303c5cc7c2029c8c3a209ef2071c56b85eae81
jatin panesiya <jatinpanesiya2003@gmail.com>
2024-01-09 18:45:54 +0530

    added robot meta tag

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Omegle",
@@ -15,9 +15,13 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.1.4/adapter.min.js"></script>
+        <meta name="robots" content="noindex" />
+        <script
+          type="text/javascript"
+          src="https://cdnjs.cloudflare.com/ajax/libs/webrtc-adapter/6.1.4/adapter.min.js"
+        ></script>
       </head>
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
